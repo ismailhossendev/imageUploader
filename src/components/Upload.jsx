33f6d75f1b2f5dd6af803ab7bcd77de3cc618531
ismailhossendev <ismailhossendev@gmail.com>
2022-11-15 20:07:10 +0600

    more responsive

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -27,15 +27,15 @@ const Upload = () => {
         alert('Copied')
     }, [])
     return (
-        <div className='relative'>
+        <div className='relative p-10 h-screen'>
             {loading &&
                 <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-violet-400 right-1/2 top-10 absolute" bis_skin_checked="1"></div>
             }
-            <div className='flex justify-center items-center h-screen w-screen'>
-                <fieldset className="lg:w-96 w-full bg-gray-800 p-5 rounded space-y-1  dark:text-gray-100">
-                    <label for="files" className="block text-sm font-medium">Attachments</label>
+            <div className='flex w-full justify-center h-full items-center'>
+
+                <div className="bg-gray-800 p-5 rounded space-y-2">
                     <form onSubmit={handleSubmit} className="">
-                        <input type="file" name="image" id="files" className="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" /> <br />
+                        <input type="file" name="image" id="files" className="px-2 w-full py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" /> <br />
                         <input type="submit" className='w-full bg-indigo-500 py-2 mt-4 rounded-md' value="UPLOAD" />
                     </form>
                     {link &&
@@ -43,16 +43,16 @@ const Upload = () => {
                             <div className="max-h-[400px] max-w-[400px]">
                                 <img className='w-full h-full rounded-lg ' src={link} alt="" />
                             </div>
-                            <p className='text-center text-white'>{link}</p>
+                            <p className='text-center max-w-[200px] mx-auto text-white'>{link}</p>
                             <CopyToClipboard onCopy={onCopy} text={link}>
                                 <button className='w-full py-2 bg-rose-600 rounded-sm text-gray-100 font-semibold font-serif hover:bg-rose-500'>Copy Link</button>
                             </CopyToClipboard>
                         </>
                     }
-                </fieldset>
+                </div>
 
             </div>
-        </div>
+        </div >
     );
 };
 
